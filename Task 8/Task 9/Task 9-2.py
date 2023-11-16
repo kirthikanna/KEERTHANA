6a689c5964f7ef42c5c63269878f3a9d5514ca36
kirthikanna <sureshkannan.gss@gmail.com>
2023-11-16 08:33:28 +0000

    Create Task 9-2.py

---
+++ Task 8/Task 9/Task 9-2.py
@@ -0,0 +1,15 @@
+# 2 Write a Python code using Lambda function to check every element of a List is an Integer or string?
+
+from functools import reduce
+
+test_list = [[4,3,1],['sdf', 6, 'it'],[8, 'good', 9]]
+
+print('The original list : ' + str(test_list))
+
+res = reduce(lambda acc, sublist: acc + [elem for elem in sublist if isinstance(elem, str)], test_list, [])
+
+print('The string instances : ' + str(res))
+
+res1 = reduce(lambda acc, sublist: acc + [elem for elem in sublist if isinstance(elem, int)], test_list, [])
+
+print('The string instances : '+ str(res1))
