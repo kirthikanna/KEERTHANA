9d58f16288718ce9df3222807f870067982b0bb8
kirthikanna <sureshkannan.gss@gmail.com>
2023-11-16 06:43:57 +0000

    Update Task 8-1.py

---
+++ Task 8/Task 8-1.py
@@ -0,0 +1,7 @@
+
+#1)CREATE A PYTHON CLASS CALLED CIRCLE WHICH WIL TAKE ARGUMENT AS A CLASS
+class circle:
+    def __init__(self,l):
+        self.l=l
+c=circle(l=['10','501','22','37','100','999','87','351'])
+print("the given list in class is",c.l)
